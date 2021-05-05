6fdaf42ea14e845d165542541ee28f112c4e2ed8
mazhaojia123 <55593798+mazhaojia123@users.noreply.github.com>
2021-05-06 03:03:15 +0800

    finish the shell

---
+++ split.h
@@ -0,0 +1,16 @@
+//
+// Created by 28320 on 2021/5/6.
+//
+
+#ifndef DIRECTORY_SPLIT_H
+#define DIRECTORY_SPLIT_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> split(const string &s);
+
+#endif //DIRECTORY_SPLIT_H
